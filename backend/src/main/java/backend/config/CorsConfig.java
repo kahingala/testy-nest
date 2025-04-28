2893676c2128684484a3f06007b6d44eef8ce450
Himashi Buddhini <159009883+Himashihbd@users.noreply.github.com>
2025-04-28 15:00:38 +0530

    fix error

--- backend/src/main/java/backend/config/CorsConfig.java
+++ backend/src/main/java/backend/config/CorsConfig.java
@@ -1,10 +1,12 @@
+package backend.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class DevBoostApplication {
+public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
